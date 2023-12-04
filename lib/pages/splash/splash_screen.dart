48bf16275d43548f94f33120d3f492680818b7cd
eslam <88260578+eslam301@users.noreply.github.com>
2023-12-04 17:57:15 +0200

    finished profile page edit on theme application

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -1,35 +1,57 @@
-
-import 'package:fitgenie/pages/sign-in/sign_in.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-
-
-
-class SplashScreen extends StatelessWidget {
+import '../sign-in/sign_in.dart';
+class SplashScreen extends StatefulWidget {
   static const String routeName = '/splash';
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double width = 20;
+  double height = 10;
+  @override
   Widget build(BuildContext context) {
+
+    var theme = Theme.of(context);
     Future.delayed(const Duration(seconds: 3), () {
-       Get.offAll(() => const SignInPage(),transition: Transition.fadeIn);
+        Get.offAll(() => const SignInPage(),transition: Transition.fadeIn);
     });
+
     return Scaffold(
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color(0xff00B418),
-              Color(0xff000000),
-            ]
+        ),
+        child:  Center(
+          child: AnimatedContainer( duration: const Duration(milliseconds: 700),
+            padding:EdgeInsets.symmetric(horizontal: width,vertical: height) ,
+            decoration: BoxDecoration(
+              color: theme.colorScheme.secondary,
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+            ),
+
+            child: Text(
+              'FitGenie',
+              style: TextStyle(
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                color: theme.primaryColor,
+                fontFamily: 'Lobster',
+                fontStyle: FontStyle.italic,
+                shadows: const [
+                  Shadow(
+                    color: Colors.black,
+                    offset: Offset(2.0, 2.0),
+                  )
+                ]
+              )
+            ),
           )
         ),
-        child: const Center(
-          child: Text(
-            'FitGenie',
-          )
-        )
       )
     );
   }
