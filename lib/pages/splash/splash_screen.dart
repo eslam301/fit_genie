db67f7f12f74e16a231c37959abc3710a89f7b5a
eslam <eslameso301@gmail.com>
2024-05-03 00:36:01 +0300

    6.3.0 enhanced UX

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -35,6 +35,7 @@ class _SplashScreenState extends State<SplashScreen> {
           const Spacer(),
           const CircularProgressIndicator(),
           const Spacer(),
+
           const Text('Version 6.3.0',style: TextStyle(color: Colors.white,fontSize: 10),),
         ],
       ),
